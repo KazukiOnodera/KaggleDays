0158d57cb55e6ed1d4824e67ad4929a8872a57c2
Kazuki Onodera <luvsic02@gmail.com>
2018-05-19 15:55:04 +0200

    update

---
+++ py/204_cossim_stop.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 19 15:14:12 2018
+
+@author: kazuki.onodera
+"""
+
+import pandas as pd
+from multiprocessing import Pool
+import utils_nlp
+import utils
+utils.start(__file__)
+#==============================================================================
+
+train = utils.read_pickles('../data/202_train')
+test  = utils.read_pickles('../data/202_test')
+
+def get_sim(s):
+    vec1, vec2, vec1_, vec2_ = s.iloc[0:300], s.iloc[300:600], s.iloc[600:900], s.iloc[900:1200]
+    
+    cosine_sim  = utils_nlp.cosine_sim(vec1, vec2)
+    cosine_sim_ = utils_nlp.cosine_sim(vec1_, vec2_)
+    
+    return cosine_sim, cosine_sim_
+
+st_time = time()
+def make_features(p):
+    if p==0:
+        df=train
+        name='train'
+    else:
+        df=test
+        name='test'
+    
+    vec_df = df.apply(get_sim, axis=1)
+    df['cosine_sim_stop'] = vec_df.apply(lambda x: x[0])
+    df['cosine_sim_stop_mean'] = vec_df.apply(lambda x: x[1])
+    
+    utils.to_pickles(result, f'../data/204_{name}', utils.SPLIT_SIZE)
+
+# =============================================================================
+# 
+# =============================================================================
+
+
+pool = Pool(2)
+pool.map(make_features, [0, 1])
+pool.close()
+
+
+#==============================================================================
+utils.end(__file__)
+
